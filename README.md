c3d6a39011e545eac00f001d56ecf15185368298
Shun Arahata <arahatashun@users.noreply.github.com>
2022-06-25 14:37:08 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# vscode-actionlint
+
+vscode-actionlint is a GitHub Actions workflow files linter for [Visual Studio Code](https://code.visualstudio.com/) that uses [actionlint](https://github.com/rhysd/actionlint).
+
+## Features
+
+![actionlintexample](img/actionlint_demo.png)
+
+## Requirements
+
+- Install [actionlint](https://github.com/rhysd/actionlint)
+- Workflow files must be located under `.github/workflows`
---
+++ src/extension.ts
@@ -0,0 +1,41 @@
+import { LinterGetOffensesFunction } from "vscode-linter-api";
+
+export interface ActiontlintOffense {
+  message: string;
+  filepath: string;
+  line: number;
+  column: number;
+  kind: string;
+  snippet: string;
+}
+
+export const getOffenses: LinterGetOffensesFunction = ({ uri, stdout }) => {
+  const regex = /.github\/workflows\/.*(.yml|.yaml)/g;
+  if (uri.path.match(regex) === null) {
+    return [];
+  }
+  stdout = stdout.replace(/^'+|'+$/gm, "");
+  const payload: ActiontlintOffense[] = JSON.parse(stdout);
+  return payload.map((offense) => {
+    const lineStart = offense.line - 1;
+    const columnStart = offense.column - 1;
+
+    // Line/column end are optional.
+    const lineEnd = lineStart;
+    const columnEnd = columnStart;
+
+    return {
+      uri,
+      lineStart,
+      columnStart,
+      lineEnd,
+      columnEnd,
+      code: offense.kind,
+      message: offense.message,
+      source: "actionlint",
+      correctable: false,
+      severity: 2,
+      docsUrl: "https://github.com/rhysd/actionlint/blob/main/docs/checks.md",
+    };
+  });
+};
